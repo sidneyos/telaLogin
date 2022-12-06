758d279f155a354df68b331316b274c9db9eec50
unknown <sidneyos@icloud.com>
2022-12-05 21:41:49 -0300

    commit inicial

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+
+export const ButtonContainer = styled.button`
+    width: 100%;
+    height: 42px;
+    background-color: #000;
+    color: #FFF;
+
+    border: 1px solid #000;
+    border-radius: 21px;
+
+    &:hover {
+        opacity: 0.6;
+        cursor:pointer;
+    }
+`
